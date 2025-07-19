8d56abcc04adb2a9f6aef17c1e71bbc81c5f2911
SecDJ <chandnanidwij@gmail.com>
2025-07-19 13:05:45 +0530

    initial commit

---
+++ src/lib/axios.js
@@ -0,0 +1,11 @@
+// axios.js
+import axios from "axios";
+
+// This will be populated by Netlify's environment variables in production
+// and by your .env file in development.
+const BASE_URL = import.meta.env.VITE_APP_API_URL;
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, // This is good, keep it for authentication
+});
